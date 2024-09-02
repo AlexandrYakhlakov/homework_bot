1623cdf1597ca6b49238a04c78986b6d0f906faf
testqasrt <ayakhlakov@srt-llc.ru>
2024-09-02 19:42:14 +0300

    создал класс конфигов

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+
+class AppConfig:
+    PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
+    PRACTICUM_API_URL = os.getenv('PRACTICUM_API_URL')
+    TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+    TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
