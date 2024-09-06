541642dc33e0c307b5406e2d0cdc8b7e2fd2c3a5
testqasrt <ayakhlakov@srt-llc.ru>
2024-09-06 13:37:37 +0300

    фикс после ревью

--- homework.py
+++ homework.py
@@ -1,9 +1,9 @@
 import logging
+import os
 import time
 from http import HTTPStatus
 from json import JSONDecodeError
 from operator import itemgetter
-import  os
 
 import requests
 from requests import RequestException
@@ -16,16 +16,21 @@ TELEGRAM_TOKEN = AppConfig.TELEGRAM_TOKEN
 TELEGRAM_CHAT_ID = AppConfig.TELEGRAM_CHAT_ID
 
 ENV_ERROR_MESSAGE = 'Missing required environment variables: "{env_name}"'
-TYPE_ERROR_MESSAGE = '"{name}" type is not "{expected_type}". "{name}" is "{actual_type}"'
+TYPE_ERROR_MESSAGE = ('"{name}" type is not "{expected_type}". '
+                      '"{name}" is "{actual_type}"')
 KEY_ERROR_MESSAGE = 'Key "{key_name}" not in "{dict_name}"'
-REQUEST_DATA_MESSAGE = 'Данные запроса: url: "{url}"; headers: "{headers}"; params: "{params}";'
+REQUEST_DATA_MESSAGE = ('Данные запроса: url: "{url}"; headers: "{headers}"; '
+                        'params: "{params}";')
 CONNECTION_ERROR_MESSAGE = 'Ошибка соединения: "{exception}"'
-UPDATE_STATUS_HOMEWORK_MESSAGE = 'Изменился статус проверки работы "{name}". {verdict}'
+UPDATE_STATUS_HOMEWORK_MESSAGE = ('Изменился статус проверки работы "{name}". '
+                                  '{verdict}')
 UNKNOWN_STATUS_HOMEWORK_MESSAGE = 'Неизвестный статус работы: {name}'
 INCORRECT_STATUS_CODE_MESSAGE = 'Некорректный status_code: {status_code}'
 RESPONSE_WITH_CODE_MESSAGE = 'Данные ответа: code: "{code}"; error: "{error}"'
-INVALID_RESPONSE_BODY_MESSAGE = ('Некорректный тип данных тела ответа, ожидается json. '
-                                 'status_code: "{status_code}"; response_body: "{body}"')
+INVALID_RESPONSE_BODY_MESSAGE = ('Некорректный тип данных тела ответа, '
+                                 'ожидается json. '
+                                 'status_code: "{status_code}"; '
+                                 'response_body: "{body}"')
 
 RETRY_PERIOD = 10 * 60
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
@@ -61,29 +66,31 @@ def send_message(bot, message):
 def get_api_answer(timestamp):
     """Запрос и получение ответа от GET /homework_statuses/."""
     params = {'from_date': timestamp}
-    request_data_message = REQUEST_DATA_MESSAGE.format(url=ENDPOINT, headers=HEADERS, params=params)
+    request_data_message = REQUEST_DATA_MESSAGE.format(
+        url=ENDPOINT, headers=HEADERS, params=params)
     try:
         response = requests.get(ENDPOINT,
                                 headers=HEADERS,
                                 params=params)
     except (RequestException, ConnectionError) as e:
-        raise ConnectionError(f'{CONNECTION_ERROR_MESSAGE.format(exception=e)}.'
-                              f' {request_data_message}')
+        raise ConnectionError(CONNECTION_ERROR_MESSAGE.format(
+            exception=e) + f'. {request_data_message}')
     try:
         response_json = response.json()
         status_code = response.status_code
         if status_code != HTTPStatus.OK:
-            message = (f'{INCORRECT_STATUS_CODE_MESSAGE.format(status_code=status_code)}. '
-                       f'{request_data_message}')
+            message = (INCORRECT_STATUS_CODE_MESSAGE.format(
+                status_code=status_code) + f'. {request_data_message}')
             if 'code' in response_json or 'error' in response_json:
                 code = response_json.get('code') or ''
                 error = response_json.get('error') or ''
                 raise ValueError(f'{message}. '
-                                 f'{RESPONSE_WITH_CODE_MESSAGE.format(code=code, error=error)}')
+                                 + RESPONSE_WITH_CODE_MESSAGE.format(
+                                     code=code, error=error))
             raise ValueError(f'{message}. Тело ответа: {response_json}')
     except JSONDecodeError:
-        raise ValueError(f'{INVALID_RESPONSE_BODY_MESSAGE.format(response.status_code, response.text)}. '
-                         f'{request_data_message}')
+        raise ValueError(INVALID_RESPONSE_BODY_MESSAGE.format(
+            response.status_code, response.text) + f'. {request_data_message}')
 
     return response_json
 
@@ -94,16 +101,24 @@ def check_response(response):
         raise TypeError(TYPE_ERROR_MESSAGE.format(
             name='response', expected_type='dict', actual_type=type(response)))
     if 'current_date' not in response:
-        raise KeyError(KEY_ERROR_MESSAGE.format(key_name='current_date', dict_name='response'))
+        raise KeyError(KEY_ERROR_MESSAGE.format(
+            key_name='current_date',
+            dict_name='response'))
     if 'homeworks' not in response:
-        raise KeyError(KEY_ERROR_MESSAGE.format(key_name='homeworks', dict_name='response'))
+        raise KeyError(KEY_ERROR_MESSAGE.format(
+            key_name='homeworks',
+            dict_name='response'))
     if not isinstance(response['homeworks'], list):
         raise TypeError(TYPE_ERROR_MESSAGE.format(
-            name='homeworks', expected_type='list', actual_type=type(response["homeworks"])))
+            name='homeworks',
+            expected_type='list',
+            actual_type=type(response["homeworks"])))
     for homework in response['homeworks']:
         if not isinstance(homework, dict):
             raise TypeError(TYPE_ERROR_MESSAGE.format(
-                name='homework', expected_type='dict', actual_type=type(homework)))
+                name='homework',
+                expected_type='dict',
+                actual_type=type(homework)))
 
 
 def parse_status(homework):
@@ -145,12 +160,13 @@ def main():
 if __name__ == '__main__':
     handlers = [logging.StreamHandler()]
     if AppConfig.APP_ENV == 'dev':
-        handlers.append(logging.FileHandler(os.path.dirname(__file__) + '/.log'))
+        handlers.append(
+            logging.FileHandler(os.path.dirname(__file__) + '/.log'))
     logging.basicConfig(
-        format='%(asctime)s - [%(levelname)s] - %(funcName)s::%(lineno)d: %(message)s',
+        format=('%(asctime)s - '
+                '[%(levelname)s] - '
+                '%(funcName)s::%(lineno)d: %(message)s'),
         level=logging.DEBUG,
         handlers=[*handlers]
     )
     main()
-
-
